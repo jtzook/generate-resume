5b61bb1b4ed45c29e9568b65a68630125944bd46
Jared Zook <jaredzook@gmail.com>
2020-09-12 09:41:32 -0700

    formatting updates

--- src/App.vue
+++ src/App.vue
@@ -15,59 +15,59 @@
 </template>
 
 <script>
-import { reactive, ref, provide } from "vue";
+  import { reactive, ref, provide } from "vue";
 
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faCamera } from "@fortawesome/free-solid-svg-icons";
+  import { library, dom } from "@fortawesome/fontawesome-svg-core";
+  import { faCamera } from "@fortawesome/free-solid-svg-icons";
 
-import Footer from "./components/Footer";
-import ContentCard from "./components/ContentCard";
+  import Footer from "./components/Footer";
+  import ContentCard from "./components/ContentCard";
 
-library.add(faCamera);
-dom.watch();
+  library.add(faCamera);
+  dom.watch();
 
-export default {
-  setup() {
-    const resumeMode = reactive(ref("classic"));
-    const toggleResumeMode = () => {
-      resumeMode === "classic" ? "modern" : "classic";
-    };
-    provide(resumeMode);
-    return {
-      resumeMode,
-      toggleResumeMode,
-    };
-  },
-  components: {
-    Footer,
-    ContentCard,
-  },
-};
+  export default {
+    setup() {
+      const resumeMode = reactive(ref("classic"));
+      const toggleResumeMode = () => {
+        resumeMode === "classic" ? "modern" : "classic";
+      };
+      provide(resumeMode);
+      return {
+        resumeMode,
+        toggleResumeMode,
+      };
+    },
+    components: {
+      Footer,
+      ContentCard,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  width: 100vw;
-  height: 100vh;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
 
 <style lang="scss" scoped>
-.body {
-  height: calc(100% - 50px);
-  display: flex;
+  .body {
+    height: calc(100% - 50px);
+    display: flex;
 
-  .side-column {
-    flex: 1 1 5%;
-  }
+    .side-column {
+      flex: 1 1 5%;
+    }
 
-  .content {
-    flex: 1 1 768px;
+    .content {
+      flex: 1 1 768px;
+    }
   }
-}
 </style>
--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -2,7 +2,7 @@
   <div class="flex-1 max-w-3xl bg-gray-100 mx-12 mt-12 mb-4 rounded-lg overflow-auto">
     <CardHeader @click="toggleResumeMode" />
     <div class="mx-6 md:mx-10 my-6 md:my-8">
-      <div @click="increment" class="p-4 md:p-6 border">
+      <div class="p-4 md:p-6 border">
         <SingleColumnResume v-show="state.resumeMode === 'classic'" />
         <MultiColumnResume v-show="state.resumeMode !== 'classic'" />
       </div>
@@ -11,28 +11,28 @@
 </template>
 
 <script>
-import { reactive } from "vue";
-import CardHeader from "./CardHeader";
-import SingleColumnResume from "./resumes/SingleColumnResume";
-import MultiColumnResume from "./resumes/MultiColumnResume";
+  import { reactive } from "vue";
+  import CardHeader from "./CardHeader";
+  import SingleColumnResume from "./resumes/SingleColumnResume";
+  import MultiColumnResume from "./resumes/MultiColumnResume";
 
-export default {
-  setup() {
-    const state = reactive({
-      resumeMode: "modern",
-    });
-    const toggleResumeMode = () => {
-      state.resumeMode = state.resumeMode === "classic" ? "modern" : "classic";
-    };
-    return {
-      state,
-      toggleResumeMode,
-    };
-  },
-  components: {
-    CardHeader,
-    SingleColumnResume,
-    MultiColumnResume,
-  },
-};
+  export default {
+    setup() {
+      const state = reactive({
+        resumeMode: "modern",
+      });
+      const toggleResumeMode = () => {
+        state.resumeMode = state.resumeMode === "classic" ? "modern" : "classic";
+      };
+      return {
+        state,
+        toggleResumeMode,
+      };
+    },
+    components: {
+      CardHeader,
+      SingleColumnResume,
+      MultiColumnResume,
+    },
+  };
 </script>
