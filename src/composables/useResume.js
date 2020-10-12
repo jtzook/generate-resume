8a9267532d8c8ad42141108f683a2b407ab1b109
Jared Zook <jaredzook@gmail.com>
2020-10-12 08:37:59 -0700

    basic html2pdf functionality

--- src/composables/useResume.js
+++ src/composables/useResume.js
@@ -1,7 +1,17 @@
+import html2pdf from 'html2pdf.js'
+
 export default function useResume() {
-  const exportToCSV = (template) => {
-    // TODO: get document node, see html2pdf-usage
-    console.log("template", template);
+  // TODO: configure jsPDF ( https://github.com/MrRio/jsPDF )
+  const exportToCSV = (element) => {
+    var opt = {
+      margin:       1,
+      filename:     'resume.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+     
+    html2pdf().from(element).set(opt).save();
   };
 
   return {
