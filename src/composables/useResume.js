91372c43cd721292b83d2605c1920ad16c914128
Jared Zook <jaredzook@gmail.com>
2020-12-20 09:54:14 -0800

    html2pdf wip

--- src/composables/useResume.js
+++ src/composables/useResume.js
@@ -1,17 +1,21 @@
-import html2pdf from 'html2pdf.js'
+import html2pdf from "html2pdf.js";
 
 export default function useResume() {
   // TODO: configure jsPDF ( https://github.com/MrRio/jsPDF )
-  const exportToCSV = (element) => {
+  const exportToCSV = async (element) => {
     var opt = {
-      margin:       1,
-      filename:     'resume.pdf',
-      image:        { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+      margin: 1,
+      filename: "resume.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
     };
-     
-    html2pdf().from(element).set(opt).save();
+
+    const pdfObject = await html2pdf().from(element)
+      // .set(opt)
+      // .save();
+
+    console.log(pdfObject)
   };
 
   return {
