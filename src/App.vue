6ab26e8e57071a29ec8e5bdce7df5ebc72fef5ea
Jared Zook <jaredzook@gmail.com>
2021-10-16 12:13:39 -0700

    grid layout WIP - nav bar

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div class="layout">
-    <div></div>
+    <div class="header"></div>
     <div></div>
     <div></div>
   </div>
@@ -19,7 +19,15 @@
 
 .layout {
   display: grid;
-  grid-template-columns: [sidebar-left] auto [content] 800px [sidebar-right] auto;
+  grid-template-columns: [sidebar-left] auto [content] 800px [sidebar-right] auto [end];
   grid-template-rows: [header] 60px [body] auto [footer] 60px;
+}
+
+.header {
+  grid-column-start: sidebar-left;
+  grid-column-end: end;
+  grid-row-start: header;
+  /* grid-row-end: 3; */
+  background-color: aqua;
 }
 </style>
